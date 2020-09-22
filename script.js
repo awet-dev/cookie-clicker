8f9be651cb6050a0a9e49133fb4a1cd761705d2d
Beardificent <cis.magito@gmail.com>
2020-09-22 10:46:38 +0200

    added variables button,label and scorecard, also arrwfunction button

---
+++ script.js
@@ -0,0 +1,8 @@
+const clickButton = document.querySelector('.button');
+let counterLabel = document.querySelector('.counter');
+let scoreCard;
+
+
+clickButton.onclick = () => {
+    scoreCard++;
+}
