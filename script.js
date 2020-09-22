58c8fc8edbad359c8d30f8ed4779f23e755fb18a
Awet Tesfay <whitezscope@gmail.com>
2020-09-22 11:17:02 +0200

    activate the button for the multiplier

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const clickButton = document.querySelector('.button');
 let counterLabel = document.querySelector('.counter');
 let scoreCard = 0;
-
-
+let multiplier = 1;
+const multiplierBtn = document.querySelector("#multiplier");
+multiplierBtn.onclick = () => {
+    multiplier++;
+    scoreCard *= multiplier
+}
 clickButton.onclick = () => {
     scoreCard++;
     counterLabel.innerHTML = `${scoreCard}`;
