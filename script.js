6d14fd6ac8b15b0791fe04192fb15c290f854a57
Beardificent <cis.magito@gmail.com>
2020-09-22 12:39:30 +0200

    we fixed the multiplier button + it's function

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ let counterLabel = document.querySelector('.counter');
 let scoreCard = 0;
 let multiplier = 1;
 const multiplierBtn = document.querySelector("#multiplier");
+
+let pointsPerClick = 0;
+
+
 multiplierBtn.onclick = () => {
-    multiplier++;
-    scoreCard *= multiplier
+    pointsPerClick = 1;
+    pointsPerClick *= multiplier;
+    multiplier += 2;
+
 }
+
+
 clickButton.onclick = () => {
     scoreCard++;
+   scoreCard += pointsPerClick;
     counterLabel.innerHTML = `${scoreCard}`;
 }
+
+/*
+
+points per click = 1
+
+button multiplier (starts at 2)
+
+click button = > points per click 2
+
+ */
